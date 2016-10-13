6282d675e813caf80a40f42ec15e87c01f4f0134
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-10-13 12:32:25 -0400

    created new house form

--- game_of_thrones/app/controllers/characters_controller.rb
+++ game_of_thrones/app/controllers/characters_controller.rb
@@ -1,2 +1,6 @@
 class CharactersController < ApplicationController
+
+  def show
+    @character = Character.find(params[:id])
+end
 end
--- game_of_thrones/app/controllers/houses_controller.rb
+++ game_of_thrones/app/controllers/houses_controller.rb
@@ -8,4 +8,17 @@ end
     @house = House.find(params[:id])
   end
 
+  def new
+    @house = House.new
+  end
+
+  def create
+    @house = House.create!(house_params)
+    redirect_to houses_path
+  end
+
+private
+  def house_params
+    params.require(:house).permit(:name, :img_url)
+  end
 end
